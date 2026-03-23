cc586503fe40723e7d2dea710680c0b548597c9c
Simonjs <34932488+truesimonjs@users.noreply.github.com>
2026-03-23 10:21:14 +0100

    flyttede kunde læsning til sin egen metode i v0 opgave

--- Chap/ADONet/Unsolved/ADORosBil_V0/Program.cs
+++ Chap/ADONet/Unsolved/ADORosBil_V0/Program.cs
@@ -2,45 +2,54 @@
 // 1) DB opsætning
 using Microsoft.Data.SqlClient;
 
-SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-builder.DataSource = "(localdb)\\MSSQLLocalDB";
-builder.InitialCatalog = "RosBilDB";
-
-
-// 2) Læs alle Kunder fra DB
-List<Kunde> kunder = new List<Kunde>();
-try
+internal class Program
 {
-	// 2a) Etablér DB-forbindelse (med brug af using-syntaksen)
-	using SqlConnection connection = new SqlConnection(builder.ConnectionString);
-	connection.Open();
+    private static void Main(string[] args)
+    {
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+        builder.DataSource = "(localdb)\\MSSQLLocalDB";
+        builder.InitialCatalog = "RosBilDB";
 
-	// 2b) Definér og udfør SQL-statement
-	SqlCommand cmd = new SqlCommand("SELECT * FROM Kunde", connection);
-	SqlDataReader reader = cmd.ExecuteReader();
 
-	// 2c) Processér de læste data
-	while (reader.Read())
-	{
-		int id = reader.GetInt32(reader.GetOrdinal("Id"));
-		string navn = reader.GetString(reader.GetOrdinal("Navn"));
-		int telefon = reader.GetInt32(reader.GetOrdinal("Telefon"));
-		bool vip = reader.GetBoolean(reader.GetOrdinal("VIP"));
+        ReadKunde(builder);
+    }
+    private static void ReadKunde(SqlConnectionStringBuilder builder)
+    {
+        // 2) Læs alle Kunder fra DB
+        List<Kunde> kunder = new List<Kunde>();
+        try
+        {
+            // 2a) Etablér DB-forbindelse (med brug af using-syntaksen)
+            using SqlConnection connection = new SqlConnection(builder.ConnectionString);
+            connection.Open();
 
-		kunder.Add(new Kunde(id, navn, telefon, vip));
-	}
+            // 2b) Definér og udfør SQL-statement
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Kunde", connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            // 2c) Processér de læste data
+            while (reader.Read())
+            {
+                int id = reader.GetInt32(reader.GetOrdinal("Id"));
+                string navn = reader.GetString(reader.GetOrdinal("Navn"));
+                int telefon = reader.GetInt32(reader.GetOrdinal("Telefon"));
+                bool vip = reader.GetBoolean(reader.GetOrdinal("VIP"));
+
+                kunder.Add(new Kunde(id, navn, telefon, vip));
+            }
+        }
+        catch (SqlException sqlEx)
+        {
+            Console.WriteLine($"SqlException under læsning fra DB : {sqlEx.Message}");
+        }
+
+        // 3) Udskriv alle Kunder (5 kunder bør blive udskrevet)
+        Console.WriteLine($"Alle Kunder ({kunder.Count} ialt)");
+        Console.WriteLine("----------------------------------");
+        foreach (Kunde kunde in kunder)
+        {
+            Console.WriteLine(kunde);
+        }
+        Console.WriteLine();
+    }
 }
-catch (SqlException sqlEx)
-{
-	Console.WriteLine($"SqlException under læsning fra DB : {sqlEx.Message}");
-}
-
-// 3) Udskriv alle Kunder (5 kunder bør blive udskrevet)
-Console.WriteLine($"Alle Kunder ({kunder.Count} ialt)");
-Console.WriteLine("----------------------------------");
-foreach (Kunde kunde in kunder)
-{
-	Console.WriteLine(kunde);
-}
-Console.WriteLine();
-

939886edbe47ca3aac7a75d034e52e963029c6f1
Simonjs <34932488+truesimonjs@users.noreply.github.com>
2026-04-16 11:55:17 +0200

    Kunde brguer nu SletPageModelBase

--- Chap/RazorPages/Unsolved/RosBilRP/Pages/Kunder/Slet.cshtml.cs
+++ Chap/RazorPages/Unsolved/RosBilRP/Pages/Kunder/Slet.cshtml.cs
@@ -2,36 +2,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RosBilRP.Models;
 using RosBilRP.Services;
+using RosBilRP.SJS;
 
 namespace RosBilRP.Pages.Kunder;
 
-public class SletModel : PageModel
+public class SletModel : SletPageModelBase<Kunde, IKundeRepository>
 {
-	private IKundeRepository _repo;
-
-	[BindProperty]
-	public Kunde Element { get; set; }
-
-	public SletModel(IKundeRepository repo)
-	{
-		_repo = repo;
-	}
-
-	public virtual IActionResult OnGet(int id)
-	{
-		Kunde? element = _repo.Read(id);
-
-		if (element == null)
-			return RedirectToPage("Error");
-
-		Element = element;
-		return Page();
-	}
-
-	public virtual IActionResult OnPost()
-	{
-		_repo.Delete(Element.Id);
-
-		return RedirectToPage("Alle");
-	}
+    public SletModel(IKundeRepository repo) : base(repo)
+    {
+    }
 }

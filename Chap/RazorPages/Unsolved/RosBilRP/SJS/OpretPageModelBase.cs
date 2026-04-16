4b5ce8c428e966a504b2fcde5143bc3e9d87ff45
Simonjs <34932488+truesimonjs@users.noreply.github.com>
2026-04-16 11:38:07 +0200

    made OpretPageModelBase
    not connected yet

--- Chap/RazorPages/Unsolved/RosBilRP/SJS/OpretPageModelBase.cs
+++ Chap/RazorPages/Unsolved/RosBilRP/SJS/OpretPageModelBase.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using RosBilRP.Models;
 using RosBilRP.Services;
 
 namespace RosBilRP.SJS
 {
     public abstract class OpretPageModelBase<T, RepositoryT> : PageModel
-        where T : IHarId
+        where T : IHarId, new()
+        
         where RepositoryT : IRepository<T>
     {
+        private RepositoryT repo;
+        [BindProperty]
+        public T Element { get; set; } = new T();
+        public virtual string ReturnPage { get => "Alle"; }
+        public OpretPageModelBase(RepositoryT repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult OnPost()
+        {
+            // Tjek om det indtastede data er validt
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Send data videre til repository
+            repo.Create(Element);
+
+            // Vend tilbage til oversigen
+            return RedirectToPage(ReturnPage);
+        }
     }
 }

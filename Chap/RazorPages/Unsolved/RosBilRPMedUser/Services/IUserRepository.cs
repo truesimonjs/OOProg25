1a96d5bbd5141d5d04a0135f8228704913e71b19
Simonjs <34932488+truesimonjs@users.noreply.github.com>
2026-04-20 11:59:54 +0200

    DefinedUserRepository

---
+++ Chap/RazorPages/Unsolved/RosBilRPMedUser/Services/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using RosBilRP.Models;
+
+namespace RosBilRP.Services
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        User? VerifyUser(string providedUsername, string providedPassword);
+        List<string> Roles { get; }
+    }
+}
---
+++ Chap/RazorPages/Unsolved/RosBilRPMedUser/Services/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using RosBilRP.Models;
+
+namespace RosBilRP.Services
+{
+    public class UserRepository : EFCRepositoryBase<User,RosBilDBContext>, IUserRepository
+    {
+        public List<string> Roles
+        {
+            get
+            {
+                return new List<string>() { "admin", "ansat" };
+            }
+        }
+        public User? VerifyUser(string providedUsername, string providedPassword)
+        {
+            User? user = All.FirstOrDefault(u=>u.Navn == providedUsername);
+
+            if (user == null || !VerifyPassword(user, providedPassword))
+            {
+                return null;
+            }
+            return user;
+            
+        
+        }
+
+        private bool VerifyPassword(User user, string providedPassword)
+        {
+            return user.Password == providedPassword;
+        }
+    }
+}
